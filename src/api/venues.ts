f83c28437810a71a70870180d0aa72e65ea5ff68
jb12-art <jbqw1@msn.com>
2026-08-31 09:16:37 +0300

    feat: refactor venues API functions and remove unnecessary comments

--- src/api/venues.ts
+++ src/api/venues.ts
@@ -1,13 +1,8 @@
 // src/api/venues.ts
 
+import type { Booking } from '../types/bookings';
 import type { Venue } from '../types/venues';
-
-/**
- * Fetches all venues from holidaze API.
- *
- * @returns Array of venues
- * @throws Error if the request fails.
- */
+import { getAuthHeaders } from './authHeaders';
 
 export async function fetchVenues(): Promise<Venue[]> {
   const response = await fetch('https://v2.api.noroff.dev/holidaze/venues');
@@ -18,4 +13,23 @@ export async function fetchVenues(): Promise<Venue[]> {
 
   const json = await response.json();
   return json.data;
+}
+
+export async function fetchVenueBookings(venueId: string): Promise<Booking[]> {
+  const response = await fetch(
+    `https://v2.api.noroff.dev/holidaze/venues/${venueId}?_bookings=true`,
+    {
+      headers: getAuthHeaders(),
+    },
+  );
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      json.errors?.[0]?.message || 'Failed to fetch venue bookings',
+    );
+  }
+
+  return json.data.bookings || [];
 }

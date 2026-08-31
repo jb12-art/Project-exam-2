d313eaf54b723770ee92b4e876b273a5656a4aec
jb12-art <jbqw1@msn.com>
2026-08-31 09:17:48 +0300

    feat: remove unused fetchBookings function and streamline createBooking implementation

--- src/api/bookings.ts
+++ src/api/bookings.ts
@@ -1,46 +1,37 @@
 // src/api/bookings.ts
 
-import type { Booking } from '../types/bookings';
 import { getAuthHeaders } from './authHeaders';
 
-export async function fetchBookings(): Promise<Booking[]> {
-  const response = await fetch(`https://v2.api.noroff.dev/holidaze/bookings`, {
-    headers: getAuthHeaders(),
-  });
-
-  const json = await response.json();
-
-  if (!response.ok) {
-    throw new Error(json.errors?.[0]?.message || 'Failed to fetch bookings');
-  }
-
-  return json.data;
-}
-
 export async function createBooking(
-  id: string,
   dateFrom: string,
   dateTo: string,
   guests: number,
+  venueId: string,
 ) {
+  // convert YYYY-MM-DD to an UTC timestamp
+  const dateFromISO = `${dateFrom}T00:00:00.000Z`;
+  const dateToISO = `${dateTo}T00:00:00.000Z`;
+
+  const bookingData = {
+    dateFrom: dateFromISO,
+    dateTo: dateToISO,
+    guests,
+    venueId,
+  };
+
   const response = await fetch('https://v2.api.noroff.dev/holidaze/bookings', {
     method: 'POST',
     headers: {
       ...getAuthHeaders(),
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      dateFrom,
-      dateTo,
-      guests,
-      id,
-    }),
+    body: JSON.stringify(bookingData),
   });
 
   const json = await response.json();
 
   if (!response.ok) {
-    throw new Error(json.errors?.[0]?.message || 'Failed to create bookin');
+    throw new Error(json.errors?.[0]?.message || 'Failed to create booking');
   }
 
   return json.data;

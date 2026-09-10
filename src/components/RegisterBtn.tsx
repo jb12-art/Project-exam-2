7fa6c7237128e3d6fb9884274763f8089ca8c877
jb12-art <jbqw1@msn.com>
2026-09-10 12:09:46 +0300

    feat: add RegisterBtn component with styled button and hover effects

--- src/components/RegisterBtn.tsx
+++ src/components/RegisterBtn.tsx
@@ -1,7 +1,12 @@
 // src/components/RegisterBtn.tsx
 
 import { Link } from 'react-router-dom';
+import styles from './RegisterBtn.module.css';
 
 export default function RegisterBtn() {
-  return <Link to="/register">Register</Link>;
+  return (
+    <Link to="/register" className={styles.registerBtn}>
+      Register
+    </Link>
+  );
 }
